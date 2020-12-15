db475f0ec3c06e1b89adf636694c66ee3bbac694
Matt Selph <matt.selph@gmail.com>
2020-12-14 22:02:53 -0500

    day 3 complete

--- day3.go
+++ day3.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"io"
 )
 
 func main() {
@@ -12,7 +13,8 @@ func main() {
 	var (
 		line string
 		trees int
-		start int = 3
+		start int 
+		solution int = 1
 	)
 
 	// file object
@@ -25,31 +27,68 @@ func main() {
 	rdr := bufio.NewReader(f)
 
 	// read the first line and throw it away
-	// since we know we're going to advance 3 spaces
+	// we will only advance forward not read anything from this line
 	line, _ = rdr.ReadString('\n')
 	line = strings.Replace(line, "\n", "", 1)
 
-	for {
-		// read the next line
-		line, err = rdr.ReadString('\n')
-		line = strings.ReplaceAll(line, "\n", "")
-		if err != nil {
-			// EOF
-			fmt.Printf("%d trees encountered\n", trees)
-			return
-		}
-
-		// if start has moved past the line
-		// repeat the pattern
-		if start > len(line)-1 {
-			start = start - len(line)
-		}
-
-		if string(line[start]) == "#" {
-			trees += 1
-		}
-
-		start += 3
+	var paths = []struct{
+		right int
+		down int
+	}{
+		{right: 1, down: 1},
+		{right: 3, down: 1},
+		{right: 5, down: 1},
+		{right: 7, down: 1},
+		{right: 1, down: 2},
 	}
 
+	for k, path := range paths {
+		start = path.right
+		trees = 0
+
+		if k > 0 {
+			// next path; reread the file
+
+			// file object
+			f, err := os.Open("day3_input.txt")
+			if err != nil {
+				panic(err)
+			}
+			rdr = bufio.NewReader(f)
+
+			// read the first line and throw it away
+			// we will only advance forward not read anything from this line
+			line, _ = rdr.ReadString('\n')
+			line = strings.Replace(line, "\n", "", 1)
+		}
+
+		Read:
+		for {
+			// read the next line(s)
+			for readCounter := 0; readCounter < path.down; readCounter++ {
+				line, err = rdr.ReadString('\n')
+				if err == io.EOF{
+					// EOF
+					fmt.Printf("%d trees encountered\n", trees)
+					solution *= trees
+					f.Close()
+					break Read
+				}
+				line = strings.ReplaceAll(line, "\n", "")
+			}
+
+			// if start has moved past the line
+			// repeat the pattern
+			if start > len(line)-1 {
+				start = start - len(line)
+			}
+
+			if string(line[start]) == "#" {
+				trees += 1
+			}
+
+			start += path.right
+		}
+	}
+	fmt.Printf("Solution: %d\n", solution)
 }
