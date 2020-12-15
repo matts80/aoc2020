8b00daf69e437a130945fc67d0a985719b143623
Matt S <matt@matts.io>
2020-12-14 19:43:50 -0500

    WIP day3

--- day3.go
+++ day3.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"io"
 )
 
 func main() {
@@ -15,41 +16,59 @@ func main() {
 		start int = 3
 	)
 
+	// variable for our different paths
+	var paths = []struct{
+		right int
+		down  int
+	}{
+		{right: 1, down: 1},
+		{right: 3, down: 1},
+		{right: 5, down: 1},
+		{right: 7, down: 1},
+		{right: 1, down: 2},
+	}
+
 	// file object
 	f, err := os.Open("day3_input.txt")
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 
 	// create a new buffered reader
 	rdr := bufio.NewReader(f)
 
 	// read the first line and throw it away
-	// since we know we're going to advance 3 spaces
+	// since we know we're going to advance spaces at the begining 
 	line, _ = rdr.ReadString('\n')
 	line = strings.Replace(line, "\n", "", 1)
 
-	for {
-		// read the next line
-		line, err = rdr.ReadString('\n')
-		line = strings.ReplaceAll(line, "\n", "")
-		if err != nil {
-			// EOF
-			fmt.Printf("%d trees encountered\n", trees)
-			return
-		}
+	// read through the file once for each path
+	for _, path := range paths {
+		start = path.right
+		trees = 0
+		for {
+			// path.down tells us how many lines to read
+			for readCounter := 0; readCounter < path.down; readCounter++ {
+				line, err = rdr.ReadString('\n')
+				if err == io.EOF {
+					// EOF
+					fmt.Printf("%d trees encountered\n", trees)
+				}
+				line = strings.ReplaceAll(line, "\n", "")
+			}
 
-		// if start has moved past the line
-		// repeat the pattern
-		if start > len(line)-1 {
-			start = start - len(line)
-		}
+			// if start has moved past the line
+			// repeat the pattern
+			if start > len(line)-1 {
+				start = start - len(line)
+			}
 
-		if string(line[start]) == "#" {
-			trees += 1
-		}
+			if string(line[start]) == "#" {
+				trees += 1
+			}
 
-		start += 3
+			start += path.right
+		}
 	}
-
 }
